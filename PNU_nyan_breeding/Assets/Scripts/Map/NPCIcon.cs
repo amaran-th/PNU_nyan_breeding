209ff7f4dc067acf41bd23f45709995c1baf5396
amaran-th <songsy405@gmail.com>
2023-01-07 00:09:06 +0900

    [feat] npc 선택 이벤트 구현 - 캔버스 해상도 설정 - 아이콘 클릭 시 이벤트 구현

--- PNU_nyan_breeding/Assets/Scripts/Map/NPCIcon.cs
+++ PNU_nyan_breeding/Assets/Scripts/Map/NPCIcon.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCIcon : MonoBehaviour
 {
+    public int index;
+    
+    private NPC data;
     // Start is called before the first frame update
     void Start()
     {
-        
+        data=MapManager.npcData[index];
+        GetComponent<Image>().sprite=Resources.Load<Sprite>(data.img_path) as Sprite;
+        Debug.Log(data.img_path);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void OnClickIcon(){
+        MapManager.currentNpc=index;
+        Debug.Log(MapManager.currentNpc);
     }
 }
