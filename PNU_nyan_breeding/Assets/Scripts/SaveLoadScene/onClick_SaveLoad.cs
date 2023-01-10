610e4b5943c407de2446102f50d6bde002bae0e1
mia2583 <mia2583@pusan.ac.kr>
2023-01-10 20:42:36 +0900

    [feat] 선택한 슬롯에 따라 세이브 로드 결정 - 선택한 위치의 슬롯에 따라 세이브 파일 이름 결정하기 - 선택한 위치의 슬롯에 따라 로드 파일 결정하기

--- PNU_nyan_breeding/Assets/Scripts/SaveLoadScene/onClick_SaveLoad.cs
+++ PNU_nyan_breeding/Assets/Scripts/SaveLoadScene/onClick_SaveLoad.cs
@@ -1,15 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+ 
+// 일단은 슬롯을 총 3개로 가정
  
 public class onClick_SaveLoad : MonoBehaviour
 {
-    public GameObject Page;
-    public GameObject SaveLoadPage;
- 
-    public void saveLoad_btn_clicked()
+    public GameObject SavePopUp;
+
+    void Start()
     {
-        Page.SetActive(false);
-        SaveLoadPage.SetActive(true);
+        
+    }
+
+    public void LoadSlot(int number)
+    {
+        DataManage.instance.slotNumber = number;
+
+        //if(isSaveFileExists[number])
+        //{
+            DataManage.instance.LoadData();
+        //}
+    }
+
+    public void SaveSlot(int number)
+    {
+        DataManage.instance.slotNumber = number;
+        DataManage.instance.SaveData();
+    }
+
+    public void GoGame()
+    {
+
     }
 }
