c5e11552c16ceb82e1a8b50ea8854d021790c55d
mia2583 <mia2583@pusan.ac.kr>
2023-01-10 20:34:31 +0900

    [feat] 데이터 세이브와 로드 작성 - 데이터 구조 임시 작성 - 데이터를 json 형태로 세이브 - json 파일을 가져와서 데이터 구조에 맞게 변환

---
+++ PNU_nyan_breeding/Assets/Scripts/SaveLoadScene/DataManage.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PlayerData
+{
+    public string name;
+    public int catType;
+}
+
+public class DataManage : MonoBehaviour
+{
+    public static DataManage instance;
+
+    public PlayerData nowPlayerData = new PlayerData();
+    public string path;
+    public int slotNumber;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;   
+        }
+        else if (instance != this)
+        {
+            Destroy(instance.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+        
+        path = Application.persistentDataPath + "/" + "save";
+    }
+    
+/*
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+*/
+
+    public void SaveData()
+    {
+        string data = JsonUtility.ToJson(nowPlayerData);
+        File.WriteAllText(path + slotNumber.ToString(), data);
+    }
+
+    public void LoadData()
+    {
+        string data = File.ReadAllText(path + slotNumber.ToString());
+        nowPlayerData = JsonUtility.FromJson<PlayerData>(data);
+    }
+
+/*
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+*/
+    public void DataClear()
+    {
+        slotNumber = -1;
+        nowPlayerData = new PlayerData();
+    }
+}
