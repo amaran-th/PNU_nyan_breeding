6148afa3e0cf367995098b34892fcb11c01a78db
phg1616 <phg1616@pusan.ac.kr>
2023-01-10 16:24:52 +0900

    [feat] 환경설정내 사운드 구현

---
+++ PNU_nyan_breeding/Assets/Scripts/SettingScene/SoundsSlider.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+/*
+public class SoundsSlider : MonoBehaviour
+{
+    public AudioMixer BGM;
+
+    public void SetLevel(float slideVal)
+    {
+        BGM.SetFloat("masterVol", Mathf.Log10(sliderVal)*20);
+    }
+
+}*/
