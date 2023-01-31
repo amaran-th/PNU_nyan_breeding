7f7a4931e99742abde62fa9d2885ab3c300ae5e6
mia2583 <mia2583@pusan.ac.kr>
2023-01-31 18:11:27 +0900

    [feat] 배드 엔딩 결과 계산 - 홈 화면으로 가는 버튼 숨기기 - 엔딩씬으로 넘어가기

---
+++ PNU_nyan_breeding/Assets/Scripts/MonthlyResultScene/ToBadEnding.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToBadEnding : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(MonthlyResultManager.playerStatData.stress_stat>=50) 
+        {
+            if(Input.GetMouseButtonDown(0)) 
+            {
+                SceneManager.LoadScene("EndingScene");
+            }
+        }
+    }
+}
