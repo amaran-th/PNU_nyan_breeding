bbd6d3ba3df3feece99e76f331d7047db298194c
amaran-th <songsy405@gmail.com>
2023-02-03 17:37:22 +0900

    [feat] 활동 진행 관련 리소스 셋팅

--- PNU_nyan_breeding/Assets/Scripts/TimeTable/ActivityController.cs
+++ PNU_nyan_breeding/Assets/Scripts/TimeTable/ActivityController.cs
@@ -3,13 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ActivityController : MonoBehaviour
 {
     public GameObject plan;
-    public GameObject firstActivity;
-    public GameObject secondActivity;
-    public GameObject thirdActivity;
+    public GameObject[] activities=new GameObject[3];
     public static bool trigger;
     void Awake(){
         trigger=false;
@@ -17,27 +16,35 @@ public class ActivityController : MonoBehaviour
     void Update(){
         if(trigger){
             trigger=false;
+            LoadIllust();
             Invoke("ViewFirstActivity",1f);
+        }
+    }
+    void LoadIllust(){
+        for(int i=0;i<3;i++){
+            //TODO Activity에 gif_path, info 추가 
+            activities[i].transform.Find("Illust").GetComponent<Image>().sprite=Resources.Load<Sprite>(CalenderController.scheduleList[i].img_path) as Sprite;
+            activities[i].transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>().text=CalenderController.scheduleList[i].name+"을 했다!";
         }
     }
 
     
     void ViewFirstActivity(){
         trigger=false;
-        firstActivity.SetActive(true);
+        activities[0].SetActive(true);
         Invoke("RemoveFirstPage",2f);
     }
     void RemoveFirstPage(){
         plan.gameObject.SetActive(false);
-        firstActivity.gameObject.SetActive(false);
+        activities[0].gameObject.SetActive(false);
         Invoke("ViewSecondActivity",1f);
     }
     void ViewSecondActivity(){
-        secondActivity.gameObject.SetActive(true);
+        activities[1].gameObject.SetActive(true);
         Invoke("ViewThirdActivity",2f);
     }
     void ViewThirdActivity(){
-        thirdActivity.gameObject.SetActive(true);
+        activities[2].gameObject.SetActive(true);
         Invoke("NextScene",2f);
     }
 
