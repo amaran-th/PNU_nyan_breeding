a96071225a42baaf98614b377cca35ae936ed605
pipisebastian <yrt7998@pusan.ac.kr>
2023-02-08 16:47:24 +0900

    [feat] 모달창 분리
    - 모달창 분리
    - 모달 UI 적용

--- PNU_nyan_breeding/Assets/Scripts/HomeScene/HomeManager.cs
+++ PNU_nyan_breeding/Assets/Scripts/HomeScene/HomeManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HomeManager : MonoBehaviour
 {
@@ -17,4 +18,26 @@ public class HomeManager : MonoBehaviour
         playerStat = playerStatData.GetStatList();
         npcData = Managers.Data.npcData;  
     }
+
+    void Start()
+    {
+        ModalManager.instance.OpenModal("1번째 튜토리얼입니다~",  YesOrNoButton );  
+    }
+
+    public void YesOrNoButton()
+    {
+         ModalManager.instance.YesOrNoOpenModal("튜토리얼 볼것인가 말것인가!",YesButton,NoButton);  
+    }
+
+    public void YesButton()
+    {
+        Debug.Log("네");
+        ///SceneManager.LoadScene("");
+    }
+
+    public void NoButton()
+    {
+        Debug.Log("아니오"); 
+    }
+
 }
