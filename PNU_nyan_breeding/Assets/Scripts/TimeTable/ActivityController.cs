649e4d52b991954349133549d2ca5f1594cdf723
suuding <tngus4789@naver.com>
2023-02-21 16:12:09 +0900

    [feat] 선택씬 일러스트 애니메이션 구현

--- PNU_nyan_breeding/Assets/Scripts/TimeTable/ActivityController.cs
+++ PNU_nyan_breeding/Assets/Scripts/TimeTable/ActivityController.cs
@@ -24,7 +24,7 @@ public class ActivityController : MonoBehaviour
         for(int i=0;i<3;i++){
             //TODO Activity에 gif_path, info 추가 
             activities[i].transform.Find("Illust").GetComponent<Image>().sprite=Resources.Load<Sprite>(CalenderController.scheduleList[i].img_path) as Sprite;
-            activities[i].transform.Find("Illust").GetComponent<Animator>().SetInteger("activity_type",2);
+            //activities[i].transform.Find("Illust").GetComponent<Animator>().SetInteger("activity_type",2);
             activities[i].transform.Find("Text (TMP)").GetComponent<TextMeshProUGUI>().text=CalenderController.scheduleList[i].name+"을 했다!";
             
         }
@@ -36,6 +36,8 @@ public class ActivityController : MonoBehaviour
     void ViewFirstActivity(){
         trigger=false;
         activities[0].SetActive(true);
+        //임시로 activity_type을 2로 넣음
+        activities[0].transform.Find("Illust").GetComponent<Animator>().SetInteger("activity_type",2);
         Invoke("RemoveFirstPage",2f);
     }
     void RemoveFirstPage(){
@@ -45,10 +47,12 @@ public class ActivityController : MonoBehaviour
     }
     void ViewSecondActivity(){
         activities[1].gameObject.SetActive(true);
+        activities[1].transform.Find("Illust").GetComponent<Animator>().SetInteger("activity_type",2);
         Invoke("ViewThirdActivity",2f);
     }
     void ViewThirdActivity(){
         activities[2].gameObject.SetActive(true);
+        activities[2].transform.Find("Illust").GetComponent<Animator>().SetInteger("activity_type",3);
         Invoke("ActiveNextButton",2f);
     }
     void ActiveNextButton(){
