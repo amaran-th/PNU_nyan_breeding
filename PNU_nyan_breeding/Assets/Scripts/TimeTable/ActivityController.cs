a5c378c72a0e79c6fba2a339a60e8386ec5628d9
suuding <tngus4789@naver.com>
2023-02-21 17:14:42 +0900

    [feat] 시간표씬에서 일러스트 fadein 애니메이션 구현

--- PNU_nyan_breeding/Assets/Scripts/TimeTable/ActivityController.cs
+++ PNU_nyan_breeding/Assets/Scripts/TimeTable/ActivityController.cs
@@ -10,6 +10,22 @@ public class ActivityController : MonoBehaviour
     public GameObject[] activities=new GameObject[3];
     public GameObject nextButton;
     public static bool trigger;
+
+    Color[] colors = new Color[3];
+    Vector3 target = new Vector3(400.26f, 800, 0);
+    private float speed=1f;
+    private float time;
+    bool[] fadeins = {true, true, true};
+
+    void Start()
+    {
+        for(int i=0;i<3;i++){
+            colors[i]=activities[i].transform.Find("Illust").GetComponent<Image>().color;
+            colors[i].a=0;
+            activities[i].transform.Find("Illust").GetComponent<Image>().color = colors[i];
+        }
+    }
+    
     void Awake(){
         trigger=false;
     }
@@ -18,6 +34,39 @@ public class ActivityController : MonoBehaviour
             trigger=false;
             LoadIllust();
             Invoke("ViewFirstActivity",1f);
+        }
+
+        if (activities[0].activeSelf && fadeins[0]) {
+            colors[0].a=1;
+            activities[0].transform.Find("Illust").GetComponent<Image>().color = colors[0]; 
+            activities[0].transform.localScale = Vector3.one * (1-time*speed);
+            if (time > 0.2f) {
+                time=0;
+                fadeins[0]=false;
+            }
+            time+=Time.deltaTime;
+        }
+
+        if (activities[1].activeSelf && fadeins[1]) {
+            colors[1].a=1;
+            activities[1].transform.Find("Illust").GetComponent<Image>().color = colors[1]; 
+            activities[1].transform.localScale = Vector3.one * (1-time*speed);
+            if (time > 0.2f) {
+                time=0;
+                fadeins[1]=false;
+            }
+            time+=Time.deltaTime;
+        }
+
+        if (activities[2].activeSelf && fadeins[2]) {
+            colors[2].a=1;
+            activities[2].transform.Find("Illust").GetComponent<Image>().color = colors[2]; 
+            activities[2].transform.localScale = Vector3.one * (1-time*speed);
+            if (time > 0.2f) {
+                time=0;
+                fadeins[2]=false;
+            }
+            time+=Time.deltaTime;
         }
     }
     void LoadIllust(){
@@ -36,8 +85,7 @@ public class ActivityController : MonoBehaviour
     void ViewFirstActivity(){
         trigger=false;
         activities[0].SetActive(true);
-        //임시로 activity_type을 2로 넣음
-        activities[0].transform.Find("Illust").GetComponent<Animator>().SetInteger("activity_type",2);
+        activities[0].transform.Find("Illust").GetComponent<Animator>().SetInteger("activity_type",2); //임시로 activity_type을 2로 넣음
         Invoke("RemoveFirstPage",2f);
     }
     void RemoveFirstPage(){
