5bfa518696777abd674cffba1f2e2d2a895cd5d9
pipisebastian <yrt7998@pusan.ac.kr>
2023-02-21 22:54:25 +0900

    [feat] : 튜토리얼 시간표씬 구현
    - 튜토리얼 홈씬 - 버튼 기능 빼기(시간표 버튼)
    - 튜토리얼 시간표씬 구현
    - 홈씬에서 튜토리얼 하겠냐는 모달 flag처리로 한번만 뜨게

--- PNU_nyan_breeding/Assets/Scripts/HomeScene/HomeManager.cs
+++ PNU_nyan_breeding/Assets/Scripts/HomeScene/HomeManager.cs
@@ -11,31 +11,36 @@ public class HomeManager : MonoBehaviour
     public static List<int> playerStat = new List<int>();
     public static Dictionary<int, NPC> npcData;
 
+    public static int flag = 0;
+
     void Awake()
     {
         playerInfoData = Managers.Player.playerInfoData;  
         playerStatData = Managers.Player.playerStatData;  
         playerStat = playerStatData.GetStatList();
         npcData = Managers.Data.npcData;  
+ 
     }
 
     void Start()
     {
        // ModalManager.instance.OpenModal("1번째 튜토리얼입니다~",  YesOrNoButton );  
-        if (playerInfoData.month == 3 && playerInfoData.grade == 1 ){
-            ModalManager.instance.YesOrNoOpenModal("튜토리얼 볼것인가 말것인가!",YesButton,NoButton);  
+        if (playerInfoData.month == 3 && playerInfoData.grade == 1 && flag == 0){
+            ModalManager.instance.YesOrNoOpenModal("튜토리얼 볼것인가 말것인가!",YesButton,NoButton);
+            flag = 1;  
         }
     }
 
     public void YesButton()
     {
         Debug.Log("네");
-         SceneManager.LoadScene("TutorialHomeScene");
+        SceneManager.LoadScene("TutorialHomeScene");
     }
 
     public void NoButton()
     {
         Debug.Log("아니오"); 
+        
     }
 
 }
