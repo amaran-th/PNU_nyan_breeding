0958ddb3e0ff87ad7d1b3e0f80263a90b9bc0421
pipisebastian <yrt7998@pusan.ac.kr>
2023-03-08 19:20:10 +0900

    feat : 스탯 숫자 표시

--- PNU_nyan_breeding/Assets/Scripts/MonthlyResultScene/ShowStat.cs
+++ PNU_nyan_breeding/Assets/Scripts/MonthlyResultScene/ShowStat.cs
@@ -7,6 +7,8 @@ using TMPro;
 public class ShowStat : MonoBehaviour
 {
     public GameObject stat; // stat prefabs
+    public TextMeshProUGUI statChange; //
+     public TextMeshProUGUI statOrigin; 
     
     void Start()
     {
@@ -17,27 +19,42 @@ public class ShowStat : MonoBehaviour
             statItem.transform.localScale = new Vector3(1.29f, 1.29f, 0f);
          
             statItem.transform.localPosition=new Vector3(0f, 1f -i*90f , 0f); 
-
-        
+            var origin = MonthlyResultManager.playerStat[i] + MonthlyResultManager.Diff[i];
+            
+            statOrigin.text += origin+"\n";
+            
+      
             if(MonthlyResultManager.Diff[i]>0){
                 statItem.transform.Find("Origin").GetComponent<Slider>().value = MonthlyResultManager.playerStat[i];
                 statItem.transform.Find("Change").GetComponent<Slider>().value = MonthlyResultManager.playerStat[i] + MonthlyResultManager.Diff[i];
+                statItem.transform.Find("Change").Find("Fill Area").Find("Fill").GetComponent<Image>().color  = new Color(0,0,255); //파란색
+
+                statChange.text += "<color=blue>+" + MonthlyResultManager.Diff[i] + "</color>\n" ;
+                
             }
             else if (MonthlyResultManager.Diff[i]<0){
                 statItem.transform.Find("Change").GetComponent<Slider>().value = MonthlyResultManager.playerStat[i];
                 statItem.transform.Find("Origin").GetComponent<Slider>().value = MonthlyResultManager.playerStat[i] + MonthlyResultManager.Diff[i];
 
-                statItem.transform.Find("Change").Find("Fill Area").Find("Fill").GetComponent<Image>().color  = new Color(0,0,255); //파란색
+                //statItem.transform.Find("Change").Find("Fill Area").Find("Fill").GetComponent<Image>().color  = new Color(0,0,255); //파란색
+                
+                statChange.text += "<color=red>" + MonthlyResultManager.Diff[i] + "</color>\n" ;
+            
             }
-            else{
+            else{ 
                 statItem.transform.Find("Origin").GetComponent<Slider>().value = MonthlyResultManager.playerStat[i];
+                statChange.text += "" + MonthlyResultManager.Diff[i] + "\n" ;
+          
             }
         }
-    }
+
+        
+    
 
     // Update is called once per frame
     void Update()
     {
         
     }
+}
 }
