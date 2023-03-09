574751f53b158172b73fc8e12590a0aca6f262ca
mia2583 <mia2583@pusan.ac.kr>
2023-03-09 21:20:07 +0900

    fix : 엔딩 컬렉션 페이지 넘기기 기능

--- PNU_nyan_breeding/Assets/Scripts/EndingCollectionScene/EndingCollectionButtons.cs
+++ PNU_nyan_breeding/Assets/Scripts/EndingCollectionScene/EndingCollectionButtons.cs
@@ -19,12 +19,14 @@ public class EndingCollectionButtons : MonoBehaviour
     {
         LeftBtn.interactable=false;
         RightBtn.interactable=true;
+        Debug.Log(JsonUtility.ToJson(EndingCollectionManager.endingCollectionData));
         endingDict = EndingCollectionManager.endingCollectionData.MakeDict();
 
-        /*for(int i=0; i<4; i++) {
-            EndingName[i].text = endingDict[page].ending[i].endingName;
-            EndingDate[i].text = endingDict[page].ending[i].done.ToString();
-        }*/
+        for(int i=0; i<4; i++) {
+            // done에 따라 이미지, 글자 활성화
+            EndingName[i].text = endingDict[0].ending[i].endingName;
+            EndingDate[i].text = endingDict[0].ending[i].done.ToString();
+        }
     }
 
     public void GoTitleButtonOnClick() 
@@ -43,27 +45,29 @@ public class EndingCollectionButtons : MonoBehaviour
             LeftBtn.interactable=true;
             RightBtn.interactable=true;
         }
-        //Debug.Log(JsonUtility.ToJson(EndingCollectionManager.endingCollectionData)); //json파일 잘 받아오는지 검사
-        //Debug.Log(endingDict[0].ending.endingName);
-        Debug.Log(JsonUtility.ToJson(endingDict[0]));
-        //string pageNumber="EndingCollectionManager.endingCollectionData";
-        //Debug.Log(JsonUtility.ToJson(pageNumber));
-        /*for(int i=0; i<4; i++) {
+        //Debug.Log(JsonUtility.ToJson(endingDict[1]));
+
+        for(int i=0; i<4; i++) {
             EndingName[i].text = endingDict[page].ending[i].endingName;
             EndingDate[i].text = endingDict[page].ending[i].done.ToString();
-        }*/
+        }
     }
 
     public void FlipPageRight()
     {
         page++;
-        if(page==4) {
+        if(page==1) {
             LeftBtn.interactable=true;
             RightBtn.interactable=false;
         }
         else {
             RightBtn.interactable=true;
             LeftBtn.interactable=true;
+        }
+
+        for(int i=0; i<4; i++) {
+            EndingName[i].text = endingDict[page].ending[i].endingName;
+            EndingDate[i].text = endingDict[page].ending[i].done.ToString();
         }
     }
 
@@ -74,9 +78,5 @@ public class EndingCollectionButtons : MonoBehaviour
 
     void Update()
     {
-        /*for(int i=0; i<4; i++) {
-            EndingName[i].text = endingDict[1].ending[i].endingName;
-            EndingDate[i].text = endingDict[1].ending[i].done.ToString();
-        }*/
     }
 }
